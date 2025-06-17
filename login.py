4ff61b994129e6d3e574b6d3413664dab57f064b
Urszula Szmit <252830@edu.p.lodz.pl>
2025-06-17 14:53:39 +0200

    Zmiana przyciskw i etykiet w ekranie logowania studenta

--- login.py
+++ login.py
@@ -23,13 +23,19 @@ def uruchom_logowanie(prev_window):
     bg = ImageTk.PhotoImage(bg_i)
     tlo.create_image(0, 0, image=bg, anchor="nw")
 
-    tk.Label(tlo, text="Login:").pack(pady=5)
-    login_entry = tk.Entry(tlo)
-    login_entry.pack()
+    tlo1_height=300
+    tlo1_width=300
+    tlo1 = tk.Canvas(tlo, width=tlo1_width, height=tlo1_height, bg="#750006")
 
-    tk.Label(tlo, text="Hasło:").pack(pady=5)
-    haslo_entry = tk.Entry(tlo, show="*")
-    haslo_entry.pack()
+    login_label=tk.Label(tlo1, text="LOGIN:", font="Georgia 16 bold", bg="#750006", fg="#d9dad9")
+    login_window = tlo1.create_window(tlo1_width/2, 50, window=login_label)
+    login_entry = tk.Entry(tlo1)
+    login_entry_window = tlo1.create_window(tlo1_width / 2, 75, window=login_entry)
+
+    haslo_label = tk.Label(tlo1, text="HASŁO:", font="Georgia 16 bold", bg="#750006", fg="#d9dad9")
+    haslo_window = tlo1.create_window(tlo1_width / 2, 100, window=haslo_label)
+    haslo_entry = tk.Entry(tlo1, show="*")
+    haslo_window = tlo1.create_window(tlo1_width / 2, 125, window=haslo_entry)
 
     def zaloguj():
         login = login_entry.get()
@@ -41,9 +47,11 @@ def uruchom_logowanie(prev_window):
         else:
             messagebox.showerror("Błąd", "Nieprawidłowy login lub hasło")
 
-    tk.Button(tlo, text="Zaloguj", command=zaloguj).pack(pady=10)
-    tk.Button(tlo, text="Powrót", command=lambda: powrot_do_ekranu_wyboru(root)).pack(pady=10)
-
+    zaloguj = tk.Button(tlo1, text="ZALOGUJ", command=zaloguj, font="Georgia 14", bg="#d9dad9", fg="#750006")#.pack(pady=10)
+    zaloguj_window = tlo1.create_window(tlo1_width/2, 200, window=zaloguj)
+    powrot = tk.Button(tlo1, text="POWRÓT", command=lambda: powrot_do_ekranu_wyboru(root), font="Georgia 14", bg="#d9dad9", fg="#750006")#.pack(pady=10)
+    powrot_window = tlo1.create_window(tlo1_width / 2, 250, window=powrot)
+    tlo1.place(x=screen_width / 2, y=screen_height /3, anchor="c")
     root.bind('<Return>', lambda event: zaloguj())
 
     root.mainloop()
