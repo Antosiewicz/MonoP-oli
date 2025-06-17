de79a5dbc9f0de8449f54a7df7d3b7b411b0d795
Urszula Szmit <252830@edu.p.lodz.pl>
2025-06-17 13:09:04 +0200

    Dodanie tła do ekranu logowania studenta

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-
+from PIL import Image, ImageTk
 from database import zaloguj_uzytkownika
 import student_window
 import login_screen
@@ -16,12 +16,19 @@ def uruchom_logowanie(prev_window):
     root.geometry(f"{screen_width}x{screen_height}")
     root.configure(bg="#e2dbd8")
 
-    tk.Label(root, text="Login:").pack(pady=5)
-    login_entry = tk.Entry(root)
+    # Tworzenie tła
+    tlo = tk.Canvas(root, width=screen_width, height=screen_height)
+    tlo.pack(fill="both", expand=True)
+    bg_i = Image.open("tlo_politechnika_kontury.png").resize((screen_width, screen_height))
+    bg = ImageTk.PhotoImage(bg_i)
+    tlo.create_image(0, 0, image=bg, anchor="nw")
+
+    tk.Label(tlo, text="Login:").pack(pady=5)
+    login_entry = tk.Entry(tlo)
     login_entry.pack()
 
-    tk.Label(root, text="Hasło:").pack(pady=5)
-    haslo_entry = tk.Entry(root, show="*")
+    tk.Label(tlo, text="Hasło:").pack(pady=5)
+    haslo_entry = tk.Entry(tlo, show="*")
     haslo_entry.pack()
 
     def zaloguj():
@@ -34,8 +41,8 @@ def uruchom_logowanie(prev_window):
         else:
             messagebox.showerror("Błąd", "Nieprawidłowy login lub hasło")
 
-    tk.Button(root, text="Zaloguj", command=zaloguj).pack(pady=10)
-    tk.Button(root, text="Powrót", command=lambda: powrot_do_ekranu_wyboru(root)).pack(pady=10)
+    tk.Button(tlo, text="Zaloguj", command=zaloguj).pack(pady=10)
+    tk.Button(tlo, text="Powrót", command=lambda: powrot_do_ekranu_wyboru(root)).pack(pady=10)
 
     root.bind('<Return>', lambda event: zaloguj())
 
