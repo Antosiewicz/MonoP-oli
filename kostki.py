076c687f7195c8072cfddc3fa2b7e745b74120f1
SzymonPokora <251205@edu.p.lodz.pl>
2025-06-17 20:07:00 +0200

    Wywołanie dźwięku po kliknięciu przycisku rzutu kostkami

--- kostki.py
+++ kostki.py
@@ -43,8 +43,11 @@ def animuj_rzut_kostkami(okno, label1, label2, grafiki, callback_wyniku=None):
     animuj()
 
 def dodaj_przycisk_rzutu(okno, label1, label2, grafiki, callback_wyniku=None):
+    def przycisk_click():
+        odtworz_dzwiek()
+        animuj_rzut_kostkami(okno, label1, label2, grafiki, callback_wyniku)
     btn = tk.Button(
         okno, text="Rzuć kostkami", font=("Inter", 20), bg="#750006", fg="white",
-        command=lambda: animuj_rzut_kostkami(okno, label1, label2, grafiki, callback_wyniku)
+        command=przycisk_click
     )
     btn.place(x=1170, y=350)
