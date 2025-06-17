a2ae32f96fd5c0261c01b2994fc06e7ef78849a3
Urszula Szmit <252830@edu.p.lodz.pl>
2025-06-17 15:34:24 +0200

    Zmiany graficzne w ekranie logowania prowadzącego

--- login.py
+++ login.py
@@ -47,7 +47,7 @@ def uruchom_logowanie(prev_window):
         else:
             messagebox.showerror("Błąd", "Nieprawidłowy login lub hasło")
 
-    zaloguj = tk.Button(tlo1, text="ZALOGUJ", command=zaloguj, font="Georgia 14", bg="#d9dad9", fg="#750006")#.pack(pady=10)
+    zaloguj = tk.Button(tlo1, text="ZALOGUJ", command=zaloguj, font="Georgia 14", bg="#d9dad9", fg="#750006")
     zaloguj_window = tlo1.create_window(tlo1_width/2, 200, window=zaloguj)
     powrot = tk.Button(tlo1, text="POWRÓT", command=lambda: powrot_do_ekranu_wyboru(root), font="Georgia 14", bg="#d9dad9", fg="#750006")#.pack(pady=10)
     powrot_window = tlo1.create_window(tlo1_width / 2, 250, window=powrot)
