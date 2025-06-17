96e17cd6f9d99476203ee706648e63d214496d37
251119 <251119@edu.p.lodz.pl>
2025-06-17 17:16:10 +0200

    Plansza dla prowadzącego + Błąd przy próbie dołączenie podczas gry

--- nieobecnosc.py
+++ nieobecnosc.py
@@ -1,11 +1,15 @@
-from PIL import ImageTk
+from PIL import ImageTk,Image
 import plansza
 from pole import *
-
+from tkinter import messagebox
 
 class Nieobecnosc(Pole):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.filename = "nieobecnosc.png"
         obraz = Image.open(self.filename).resize((plansza.POLE_X, plansza.POLE_Y))
-        self.photo = ImageTk.PhotoImage(obraz)
+        self.photo = ImageTk.PhotoImage(
+        Image.open(self.filename).resize((plansza.POLE_X, plansza.POLE_Y))
+    )
+    def akcja(self, gracz):
+        messagebox.showwarning("Brak obecności", "Nieobecność – brak punktów za to pole.")
