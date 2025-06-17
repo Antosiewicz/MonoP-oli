c39912256703ad8bb8e715ec7df8fc37e2bcd0de
Urszula Szmit <252830@edu.p.lodz.pl>
2025-06-17 16:23:29 +0200

    Poprawa logowania enterem

--- prowadzacy_login.py
+++ prowadzacy_login.py
@@ -47,8 +47,8 @@ def uruchom_logowanie_prowadzacy(prev_window):
         else:
             messagebox.showerror("Błąd", "Nieprawidłowy login lub hasło")
 
-    zaloguj = tk.Button(tlo1, text="ZALOGUJ", command=zaloguj, font="Georgia 14", bg="#d9dad9", fg="#750006")
-    zaloguj_window = tlo1.create_window(tlo1_width / 2, 200, window=zaloguj)
+    zaloguj_button = tk.Button(tlo1, text="ZALOGUJ", command=zaloguj, font="Georgia 14", bg="#d9dad9", fg="#750006")
+    zaloguj_window = tlo1.create_window(tlo1_width / 2, 200, window=zaloguj_button)
     powrot = tk.Button(root, text="Powrót", command=lambda: powrot_do_menu(root), font="Georgia 14", bg="#d9dad9", fg="#750006")
     powrot_window = tlo1.create_window(tlo1_width / 2, 250, window=powrot)
     tlo1.place(x=screen_width / 2, y=screen_height / 3, anchor="c")
