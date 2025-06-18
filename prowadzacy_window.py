f8d03cd2d5f16e79e0c59e3c98246b4ea873a4da
Antosiewicz <157109309+Antosiewicz@users.noreply.github.com>
2025-06-18 10:53:57 +0200

    Update prowadzacy_window.py

--- prowadzacy_window.py
+++ prowadzacy_window.py
@@ -46,7 +46,7 @@ def uruchom_okno_prowadzacy():
         width=20,
         height=1
     )
-    edytuj_button.place(x=screen_width/2-120, y=400)
+    edytuj_button.place(x=screen_width/2-120, y=550)
 
     def start_gra():
         try:
@@ -63,7 +63,7 @@ def uruchom_okno_prowadzacy():
             json.dump(dane, f, indent=2)
             
 
-    tk.Button(okno, text="START GRY", command=start_gra, fg="#d9dad9", bg="#750006", font='Georgia', width=20, height=1).place(x=screen_width/2-120, y=300)
+    tk.Button(okno, text="START GRY", command=start_gra, fg="#d9dad9", bg="#750006", font='Georgia', width=20, height=1).place(x=screen_width/2-120, y=450)
 
     def reset_gra():
         with open("gra_status.json", "w", encoding="utf-8") as f:
@@ -76,7 +76,7 @@ def uruchom_okno_prowadzacy():
                     pass
         pionki_graczy.clear()
 
-    tk.Button(okno, text="RESET GRY", command=reset_gra, fg="#d9dad9", bg="#750006", font='Georgia', width=20, height=1).place(x=screen_width/2-120, y=350)
+    tk.Button(okno, text="RESET GRY", command=reset_gra, fg="#d9dad9", bg="#750006", font='Georgia', width=20, height=1).place(x=screen_width/2-120, y=500)
 
     def on_closing():
         with open("gra_status.json", "w", encoding="utf-8") as f:
